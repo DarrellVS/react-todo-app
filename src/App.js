186c3240f936256fce1f47e09aba8c8a22da9c01
DarrellVS <darrell260802@gmail.com>
2021-08-31 20:15:37 +0200

    Redesign

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
 
+import './css/App.css';
+
 const LOCAL_STORAGE_KEY = 'todosApp.todos'
 
 function App() {
@@ -43,13 +45,26 @@ function App() {
 
   // Return a populated todolist and corresponding inputs
   return (
-    <>
+    <main className="container">
+      <h1 className="page-header">Darrell's Todo List</h1>
+      <div className="counter">{todos.filter(todo => !todo.complete).length} left to do</div>
+
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearComplete}>Clear Completed Todos</button>
-      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
-    </>
+
+      <div className="inputs">
+        <input ref={todoNameRef} type="text" />
+        <button onClick={handleAddTodo}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+          </svg>
+        </button>
+        <button onClick={handleClearComplete}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eraser" viewBox="0 0 16 16">
+            <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z" />
+          </svg>
+        </button>
+      </div>
+    </main>
   )
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,8 +3,10 @@ import Todo from './Todo'
 
 export default function TodoList({ todos, toggleTodo }) {
     return (
-        todos.map(todo => {
-            return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
-        })
+        <div className="todoList">
+            {todos.map(todo => {
+                return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+            })}
+        </div>
     )
 }
