cc24b3788891d56cd6179c5fdde6d2b37713cb04
DarrellVS <darrell260802@gmail.com>
2021-09-01 15:05:34 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
 import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
+import { createAvatar } from '@dicebear/avatars'
+import * as style from '@dicebear/croodles'
+import './css/App.css'
 
-import './css/App.css';
+// User ID Handling
+const userID = localStorage.getItem('todosApp.userID')
+if (!userID) localStorage.setItem('todosApp.userID', uuidv4())
+let svg = generateSVG(userID)
 
-const LOCAL_STORAGE_KEY = 'todosApp.todos'
+function generateSVG(seed) {
+  return createAvatar(style, {
+    seed: seed,
+  })
+}
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+  const [, updateState] = React.useState()
+  const forceUpdate = React.useCallback(() => updateState({}), [])
 
   // Load Stored Todos
   useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    const storedTodos = JSON.parse(localStorage.getItem('todosApp.todos'))
     if (storedTodos) setTodos(storedTodos)
   }, [])
 
   // Store todos upon array change
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    localStorage.setItem('todosApp.todos', JSON.stringify(todos))
   }, [todos])
+
+  // New User ID
+  function generateNewUserID() {
+    const newUserID = uuidv4()
+    localStorage.setItem('todosApp.userID', newUserID)
+    svg = generateSVG(newUserID)
+    forceUpdate()
+  }
 
   // Add a todo element to the todos array
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    setTodos(prevTodos => [...prevTodos, { id: uuidv4(), name: name, complete: false }])
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: uuidv4(), name: name, complete: false },
+    ])
     todoNameRef.current.value = null
   }
 
   // Clear completed todos from array
   function handleClearComplete(e) {
-    const newTodos = todos.filter(todo => !todo.complete)
+    const newTodos = todos.filter((todo) => !todo.complete)
     setTodos(newTodos)
   }
 
   // Toggle a todo from complete to incomplete and vice versa
   function toggleTodo(id) {
-    const newTodos = [...todos];
-    const todo = newTodos.find(todo => todo.id === id)
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo) => todo.id === id)
     todo.complete = !todo.complete
     setTodos(newTodos)
   }
@@ -46,20 +69,39 @@ function App() {
   // Return a populated todolist and corresponding inputs
   return (
     <main className="container">
-      <h1 className="page-header">Darrell's Todo List</h1>
-      <div className="counter">{todos.filter(todo => !todo.complete).length} left to do</div>
+      <h1 className="page-header" onClick={generateNewUserID}>
+        <div dangerouslySetInnerHTML={{ __html: svg }}></div>
+        Darrell's Todo List
+      </h1>
+      <div className="counter">
+        {todos.filter((todo) => !todo.complete).length} left to do
+      </div>
 
       <TodoList todos={todos} toggleTodo={toggleTodo} />
 
       <div className="inputs">
         <input ref={todoNameRef} type="text" />
         <button onClick={handleAddTodo}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-plus-lg"
+            viewBox="0 0 16 16"
+          >
             <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
           </svg>
         </button>
         <button onClick={handleClearComplete}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eraser" viewBox="0 0 16 16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-eraser"
+            viewBox="0 0 16 16"
+          >
             <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm2.121.707a1 1 0 0 0-1.414 0L4.16 7.547l5.293 5.293 4.633-4.633a1 1 0 0 0 0-1.414l-3.879-3.879zM8.746 13.547 3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z" />
           </svg>
         </button>
@@ -68,4 +110,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
